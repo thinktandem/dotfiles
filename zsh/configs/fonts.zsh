50db4d97b5bc13420db3851057c08757ab1d6cb6
Dustin LeBlanc <dustin.m.leblanc@gmail.com>
2018-10-27 23:10:43 -0400

    Sweet customizations to fonts and prompt, some path edits

---
+++ zsh/configs/fonts.zsh
@@ -0,0 +1,7 @@
+if [ -f ~/.fonts/fontawesome-regular.sh ]; then
+  source ~/.fonts/fontawesome-regular.sh
+fi
+
+if [ -f ~/.fonts/devicons-regular.sh ]; then
+  source ~/.fonts/devicons-regular.sh
+fi
--- zsh/configs/prompt.zsh
+++ zsh/configs/prompt.zsh
@@ -2,7 +2,15 @@
 git_prompt_info() {
   current_branch=$(git current-branch 2> /dev/null)
   if [[ -n $current_branch ]]; then
-    echo " %{$fg_bold[green]%}$current_branch%{$reset_color%}"
+    echo " \u$CODEPOINT_OF_AWESOME_GITHUB %{$fg_bold[blue]%}$current_branch%{$reset_color%}"
+  fi
+}
+
+nodenv_prompt_info() {
+  current_node=$(nodenv local 2> /dev/null)
+  if [[ -n $current_node ]]; then
+    echo "\u$CODEPOINT_OF_DEVICONS_NODEJS_SMALL \
+%{$fg_bold[green]%}$current_node%{$reset_color%}"
   fi
 }
 
@@ -10,5 +18,7 @@ setopt promptsubst
 
 # Allow exported PS1 variable to override default prompt.
 if ! env | grep -q '^PS1='; then
-  PS1='${SSH_CONNECTION+"%{$fg_bold[green]%}%n@%m:"}%{$fg_bold[blue]%}%c%{$reset_color%}$(git_prompt_info) %# '
+  PS1='${SSH_CONNECTION+"%{$fg_bold[green]%}%n@%m:"}%{$fg_bold[blue]%}%c%{$reset_color%}$(git_prompt_info)\
+ $(nodenv_prompt_info)
+%{$fg_bold[red]%}$(echo "\u$CODEPOINT_OF_AWESOME_REBEL") %{$reset_color%} '
 fi
